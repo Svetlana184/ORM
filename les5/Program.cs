412fc2199d5a2186f4afda5ba04894a45cfebb67
Svetlana184 <sveta.kgd.39@gmail.com>
2025-01-23 11:48:36 +0200

    les6

---
+++ les5/Program.cs
@@ -0,0 +1,72 @@
+﻿Console.WriteLine("введите x");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("введите y");
+double y1 = double.Parse(Console.ReadLine()!);
+
+Number n = new Number(x1,y1);
+
+Console.WriteLine($"{n.X} + {n.Y} = {n.Add()}");
+Console.WriteLine($"{n.X} - {n.Y} = {n.Sub()}");
+Console.WriteLine($"{n.X} * {n.Y} = {n.Mult()}");
+Console.WriteLine($"{n.X} / {n.Y} = {n.Div():F2}");
+Console.WriteLine("введите x");
+double x2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("введите y");
+double y2 = double.Parse(Console.ReadLine()!);
+
+Number n2 = new Number(x2,y2);
+
+Console.WriteLine(n + n2);
+
+
+public interface IArOperation
+{
+    double Add();
+    double Sub();
+    double Mult();
+    double Div();
+}
+
+public interface ISqrSqrt
+{
+    double Sqr();
+    double Sqrt();
+}
+
+class Number : IArOperation, ISqrSqrt
+{
+    double x, y;
+    public double X
+    {
+        get => x; 
+        set { x = value; }
+    }
+    public double Y
+    {
+        get => y;
+        set { y = value; }
+    }
+    public Number(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public double Add() => x + y;
+
+    public double Sub() => x - y;
+
+    public double Mult() => x * y;
+
+    public double Div()=> x / y;
+
+    public double Sqr()=>x*x + y*y;
+
+    public double Sqrt()=> Math.Sqrt(x*x + y*y);
+
+    public static double operator +(Number a, Number b)
+    {
+        return a.X * b.X + a.Y * b.Y;
+    }
+
+}
---
+++ les6/Program.cs
@@ -0,0 +1,87 @@
+﻿// ОБОБЩЁННЫЕ ТИПЫ ДАННЫХ (универсальные шаблоны)
+
+/*generics - классы, структуры, интерфейсы и методы, в которых некоторые типы сами являются параметрами
+ * универсальные шаблоны позволяют точно настроить метод, класс. структуру или интерфейс в соответствии с типом обрабатываемых данных
+ * 
+ */
+
+
+//Console.WriteLine("введите х");
+//var x = Console.ReadLine();
+//Console.WriteLine("введите y");
+//var y = Console.ReadLine();
+//Console.WriteLine($"x = {x}, y = {y}");
+//Swap(ref x, ref y);
+//Console.WriteLine($"x = {x}, y = {y}");
+
+//void Swap (ref int a, ref int b)
+//{
+//    int temp = a; 
+//    a = b; 
+//    b = temp;
+//}
+
+//void SwapDouble(ref double a, ref double b)
+//{
+//    double temp = a;
+//    a = b;
+//    b = temp;
+//}
+
+//void Swap<T>(ref T a, ref T b)
+//{
+//    T temp = a;
+//    a = b;
+//    b = temp;
+//}
+
+//ОГРАНИЧЕНИЯ ОБОБЩЕНИЙ
+
+
+//SendMessage(new EmailMessage("hello from email"));
+//SendMessage(new TelegramMessage("hello from telegram"));
+//SendMessage(new Haker("хех")); - ошибка
+Messanger<Message> email = new Messanger<Message>();
+email.SendMessage(new Message("hello from email"));
+Messanger<Message> telegram = new Messanger<Message>();
+telegram.SendMessage(new Message("hello from email"));
+//void SendMessage<T>(T message)  where T : Message
+//{
+//    Console.WriteLine($"Отправляется сообщение: {message.Text}");
+//}
+
+class Message
+{
+    public string Text { get; set; }
+    public Message(string text)
+    {
+        Text = text;
+    }
+
+}
+class Messanger<T> where T : Message
+{
+    public void SendMessage(T message)
+    {
+        Console.WriteLine($"Отправляется сообщение: {message.Text}");
+    }
+}
+class EmailMessage : Message
+{
+    public EmailMessage(string text) : base(text) { }
+}
+
+class TelegramMessage : Message
+{
+    public TelegramMessage(string text) : base(text) { }
+}
+
+class Haker
+{
+    public string Text { get; set; }
+    public Haker(string text)
+    {
+        Text = text;
+    }
+    public void Destroy() { }
+}
