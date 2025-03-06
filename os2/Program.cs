e88686ab58a99a9e82ba3bd529bb554becfcc4e5
Svetlana184 <sveta.kgd.39@gmail.com>
2025-03-06 14:19:20 +0200

    domain

--- os2/Program.cs
+++ os2/Program.cs
@@ -5,9 +5,10 @@
  */
 
 using System.Diagnostics;
+using System.Reflection;
 
 
-var process = Process.GetCurrentProcess();
+//var process = Process.GetCurrentProcess();
 //Console.WriteLine(process.Id);
 //Console.WriteLine(process.Handle);
 //Console.WriteLine(process.MachineName);
@@ -31,15 +32,47 @@ var process = Process.GetCurrentProcess();
 //    Console.WriteLine($"Id: {pr.Id}, Name: {pr.ProcessName}");
 //}
 
-Process[] vsproc = Process.GetProcessesByName("CalculatorApp");
-foreach (Process pr in vsproc) pr.Kill();
+//Process[] vsproc = Process.GetProcessesByName("CalculatorApp");
+//foreach (Process pr in vsproc) pr.Kill();
 
-Process calcProc = new Process();
-calcProc.StartInfo = new ProcessStartInfo("calc.exe");
-calcProc.Start();
+//Process calcProc = new Process();
+//calcProc.StartInfo = new ProcessStartInfo("calc.exe");
+//calcProc.Start();
 
-vsproc = Process.GetProcessesByName("devenv");
-foreach (Process pr in vsproc)
+//vsproc = Process.GetProcessesByName("devenv");
+//foreach (Process pr in vsproc)
+//{
+//    pr.Kill();
+//}
+
+
+//Process vsproc = Process.GetProcessesByName("devenv")[0];
+//ProcessThreadCollection processThreads = vsproc.Threads;
+//ProcessModuleCollection modules = vsproc.Modules;
+//foreach (ProcessThread thread in processThreads)
+//{
+//    Console.WriteLine($"ThreadId: {thread.Id}");
+//}
+//foreach (ProcessModule module in modules)
+//{
+//    Console.WriteLine($"{module.ModuleName} Filename: {module.FileName}");
+//}
+
+//Process.Start(@"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", "https://mail.ru");
+
+//ДОМЕНЫ ПРИЛОЖЕНИЙ
+/*при запуске приложений, написанных на си шарп, ос создает процесс, а среда common language runtime (clr)
+ * создает внутри этого процесса логический контейнер, который называется доменом приложения и 
+ * внутри которого работает запущенное приложение
+ * 
+ * 
+ */
+
+AppDomain domain = AppDomain.CurrentDomain;
+Console.WriteLine(domain.FriendlyName);
+Console.WriteLine(domain.BaseDirectory);
+Assembly[] assemblies = domain.GetAssemblies();
+foreach (Assembly assembly in assemblies)
 {
-    pr.Kill();
+    Console.WriteLine(assembly.GetName().Name);
 }
