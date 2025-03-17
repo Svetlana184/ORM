609ad0179cb4204dbe32158a9ab578b51669a668
Svetlana184 <sveta.kgd.39@gmail.com>
2025-03-17 17:59:12 +0200

    calculator

---
+++ Les22/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Les22
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string opertaion = (sender as Button).Content.ToString();
+            double var1 = double.Parse(First.Text);
+            double var2 = double.Parse(Second.Text);
+            double result = 0;
+            switch (opertaion)
+            {
+                case "+":
+                    result = var1 + var2;
+                    break;
+                case "-":
+                    result = var1 - var2;
+                    break;
+                case "*":
+                    result = var1 * var2;
+                    break;
+                case ":":
+                    result = var1 / var2;
+                    break;
+                default: 
+                    break;
+            }
+            Result.Content = $"{var1} {opertaion} {var2} = {result:F2}";
+
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string text = textBoxMessage.Text;
+        //    labelMessage.Content = text;
+        //}
+    }
+}
---
+++ les21/Program.cs
@@ -0,0 +1,16 @@
+﻿//графический интерфейс с#
+//1. winforms
+//2. wpf, xaml - язык разметки (windows)
+//3. avalonia (windows/linux)
+//4. .Net Maui (не работает под линукс)
+//5. Xamarin
+//6. UWP (universal windows platform)
+
+
+/*ТЕХНОЛОГИЯ WPF
+ * компоновка:
+ * canvas
+ * stackpanel
+ * 
+ * 
+ */
