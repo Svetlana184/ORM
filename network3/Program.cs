6d7b277d8e8cf03316f5ab67e1263b0d74fecc67
Svetlana184 <sveta.kgd.39@gmail.com>
2025-03-19 16:38:28 +0200

    clientserver

---
+++ network3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+IPEndPoint ipPoint = new IPEndPoint(IPAddress.Loopback, 8888);
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+socket.Bind(ipPoint);
+
+Console.WriteLine(socket.LocalEndPoint);
+
+socket.Listen(1000); //1000 - количество входящих подключений, которые можно уместить в очередь
+Console.WriteLine("Сервер подключен");
+
+using Socket client = await socket.AcceptAsync();
+Console.WriteLine("адрес клиента:" + client.RemoteEndPoint);
---
+++ network3Client/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Net.Sockets;
+
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    await socket.ConnectAsync("192.168.113.2", 8888);
+    Console.WriteLine($"подключение к {socket.RemoteEndPoint} установлено");
+}
+catch
+{
+    Console.WriteLine($"подключение к {socket.RemoteEndPoint} не установлено(((((");
+}
