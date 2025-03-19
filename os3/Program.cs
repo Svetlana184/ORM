4a6b2a64e83960cee83f3b14ba271b061d11b3f3
Svetlana184 <sveta.kgd.39@gmail.com>
2025-03-19 18:10:12 +0200

    ttt

--- os3/Program.cs
+++ os3/Program.cs
@@ -53,55 +53,133 @@
 //    }     
 //}
 
+
 //system.Threading.Lock
 //класс Lock и синхронизация
-int x = 0;
-Lock lockObj = new();
-for (int i = 0; i < 6; i++)
+//int x = 0;
+//Lock lockObj = new();
+//for (int i = 0; i < 6; i++)
+//{
+//    Thread myThread = new(Print);
+//    myThread.Name = $"Поток: {i}";
+//    myThread.Start();
+//}
+//void Print()
+//{
+//lock (lockObj)
+//{
+//    x = 1;
+//    for (int i = 0; i < 6; i++)
+//    {
+//        Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
+//        x++;
+//        Thread.Sleep(100);
+//    }
+//}
+//if (lockObj.TryEnter())
+//{
+//    try
+//    {
+//        x = 1;
+//        for (int i = 0; i < 6; i++)
+//        {
+//            Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
+//            x++;
+//            Thread.Sleep(100);
+//        }
+//    }
+//    finally
+//    {
+//        lockObj.Exit();
+//    }
+//}
+//    using (lockObj.EnterScope())
+//    {
+//        x = 1;
+//        for (int i = 0; i < 6; i++)
+//        {
+//            Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
+//            x++;
+//            Thread.Sleep(100);
+//        }
+//    }
+//}
+
+
+//СОБЫТИЯ (AutoResetEvent)
+//int x = 0;
+//AutoResetEvent waitHandler = new AutoResetEvent(true);
+//for (int i = 0; i < 6; i++)
+//{
+//    Thread myThread = new(Print);
+//    myThread.Name = $"Поток: {i}";
+//    myThread.Start();
+//}
+//void Print()
+//{
+//    waitHandler.WaitOne();
+//        x = 1;
+//        for (int i = 0; i < 6; i++)
+//        {
+//            Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
+//            x++;
+//            Thread.Sleep(100);
+//        }
+//    waitHandler.Set();
+//}
+
+
+//Мьютекс
+//int x = 0;
+//Mutex mutexObj = new();
+//for (int i = 0; i < 6; i++)
+//{
+//    Thread myThread = new(Print);
+//    myThread.Name = $"Поток: {i}";
+//    myThread.Start();
+//}
+//void Print()
+//{
+//    mutexObj.WaitOne();
+//    x = 1;
+//    for (int i = 0; i < 6; i++)
+//    {
+//        Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
+//        x++;
+//        Thread.Sleep(100);
+//    }
+//    mutexObj.ReleaseMutex();
+//}
+
+
+//Семафоры
+//задача про читателей
+
+class Reader
 {
-    Thread myThread = new(Print);
-    myThread.Name = $"Поток: {i}";
-    myThread.Start();
-}
-void Print()
-{
-    //lock (lockObj)
-    //{
-    //    x = 1;
-    //    for (int i = 0; i < 6; i++)
-    //    {
-    //        Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
-    //        x++;
-    //        Thread.Sleep(100);
-    //    }
-    //}
-    //if (lockObj.TryEnter())
-    //{
-    //    try
-    //    {
-    //        x = 1;
-    //        for (int i = 0; i < 6; i++)
-    //        {
-    //            Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
-    //            x++;
-    //            Thread.Sleep(100);
-    //        }
-    //    }
-    //    finally
-    //    {
-    //        lockObj.Exit();
-    //    }
-    //}
-    using (lockObj.EnterScope())
+    static Semaphore sem = new Semaphore(3, 3);
+    Thread myThread;
+    int count = 3;
+
+    public Reader(int i)
     {
-        x = 1;
-        for (int i = 0; i < 6; i++)
+        myThread = new Thread(Read);
+        myThread.Name = $"Reader {i}";
+        myThread.Start();
+    }
+    public void Read()
+    {
+        while (count > 0)
         {
-            Console.WriteLine($"{Thread.CurrentThread.Name} : {x}");
-            x++;
-            Thread.Sleep(100);
+            sem.WaitOne();
+            Console.WriteLine($"{Thread.CurrentThread.Name} goes");
+            Console.WriteLine($"{Thread.CurrentThread.Name} reads");
+            Thread.Sleep( 1000 );
+            Console.WriteLine($"{Thread.CurrentThread.Name} goes away");
+            sem.Release();
+            count--;
+            Thread.Sleep( 1000 );
+
         }
     }
-
-
 }
