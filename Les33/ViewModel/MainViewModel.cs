fa8de6e256c5a45df267e172d97fcc5a912b86f1
Svetlana184 <sveta.kgd.39@gmail.com>
2025-04-02 16:06:30 +0200

    ttt

--- Les33/ViewModel/MainViewModel.cs
+++ Les33/ViewModel/MainViewModel.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Les33.Model;
 
 namespace Les33.ViewModel
 {
-    internal class MainViewModel
+    public class MainViewModel:INotifyPropertyChanged
     {
+        private Phone selectedPhone;
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set { selectedPhone = value; }
+        }
+        public MainViewModel()
+        {
+
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title = "Samsung Galaxy", Price = 30000, Company = "Samsung" },
+                new Phone { Title = "NX400", Price = 30000, Company = "Xiaomi" },
+                new Phone { Title = "IPhone 15", Price = 150000, Company = "Apple" }
+            };
+                
+
+                public ObservableCollection<Phone> Phones
+                {
+                    get; set;
+                }
+
+
+                public event PropertyChangedEventHandler? PropertyChanged;
+
+                public void OnPropertyChanged([CallerMemberName] string prop = "")
+                {
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs(prop));
+                    }
+                }
+        }
+        
     }
 }
