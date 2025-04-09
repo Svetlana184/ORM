3b39afd27bfe5021d06f9edabd619882509c35a1
Svetlana184 <sveta.kgd.39@gmail.com>
2025-04-09 14:52:57 +0200

    clienttt

---
+++ network7Clientt/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Net.Sockets;
+
+using TcpClient tcpclient = new TcpClient();
+Console.WriteLine("started");
+await tcpclient.ConnectAsync("127.0.0.1", 8888);
+if (tcpclient.Connected) Console.WriteLine($"подключение установлено {tcpclient.Client.RemoteEndPoint}");
+else Console.WriteLine("не удалось подключиться");
