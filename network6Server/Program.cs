54ddf910f261bf4f7e9f5dc051d14f0343d34163
Svetlana184 <sveta.kgd.39@gmail.com>
2025-04-09 17:05:41 +0200

    client

--- network6Server/Program.cs
+++ network6Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 var tcpListener = new TcpListener(IPAddress.Any, 8888);
 try
@@ -9,7 +10,11 @@ try
     while (true)
     {
         using var tcpClient = await tcpListener.AcceptTcpClientAsync();
-        Console.WriteLine($"Входящее подключение: {tcpClient.Client.RemoteEndPoint}");
+        //Console.WriteLine($"Входящее подключение: {tcpClient.Client.RemoteEndPoint}");
+        var stream = tcpClient.GetStream();
+        byte[] data = Encoding.UTF8.GetBytes(DateTime.Now.ToLongTimeString());
+        await stream.WriteAsync(data);
+        Console.WriteLine($"клиенту {tcpClient.Client.RemoteEndPoint} отправлены данные ");
     }
 }
 finally
--- network7Clientt/Program.cs
+++ network7Clientt/Program.cs
@@ -1,7 +1,14 @@
 ﻿using System.Net.Sockets;
+using System.Text;
 
 using TcpClient tcpclient = new TcpClient();
 Console.WriteLine("started");
-await tcpclient.ConnectAsync("127.0.0.1", 8888);
-if (tcpclient.Connected) Console.WriteLine($"подключение установлено {tcpclient.Client.RemoteEndPoint}");
-else Console.WriteLine("не удалось подключиться");
+await tcpclient.ConnectAsync("192.168.110.60", 8888);
+//if (tcpclient.Connected) Console.WriteLine($"подключение установлено {tcpclient.Client.RemoteEndPoint}");
+//else Console.WriteLine("не удалось подключиться");
+
+byte[] data = new byte[512];
+var stream = tcpclient.GetStream();
+int bytes = await stream.ReadAsync(data);
+string time = Encoding.UTF8.GetString(data, 0, bytes);
+Console.WriteLine("текущее время " + time);
