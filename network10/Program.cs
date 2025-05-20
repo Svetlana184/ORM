1773534d0b431fae35e1fffae1827547d8d9f170
Svetlana184 <sveta.kgd.39@gmail.com>
2025-05-20 13:11:03 +0200

    nte

---
+++ network10/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+string message = "Hello METANIT.COM";
+byte[] data = Encoding.UTF8.GetBytes(message);
+EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+int bytes = await udpSocket.SendToAsync(data, remotePoint);
+Console.WriteLine($"Отправлено {bytes} байт");
